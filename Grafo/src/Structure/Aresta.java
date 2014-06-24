aa4d64cfb6b780f26c8df2d0958d028ed72fc99d
SimorC <rafaelsimor@gmail.com>
2014-06-24 09:27:09 -0300

    Melhoria de Estruturas
    + Construtores
    + Cálculo de distância da aresta

--- Grafo/src/Structure/Aresta.java
+++ Grafo/src/Structure/Aresta.java
@@ -1,5 +1,7 @@
 package Structure;
 
+import java.lang.Math;
+
 /**
  * Define uma Aresta
  * @author Simor / Vanessa C. / Larson R.
@@ -9,4 +11,20 @@ public class Aresta {
 	public Vertice A;
 	public Vertice B;
 	public double Custo;
+	public double Distancia;
+	
+	/**
+	 * Construtor
+	 * @param a Vertice A
+	 * @param b Vertice B
+	 * @param custo Custo da Aresta
+	 */
+	public Aresta(Vertice a, Vertice b, double custo) {
+		this.A = a;
+		this.B = b;
+		this.Custo = custo;
+		
+		// Calcula distância
+		this.Distancia = Math.sqrt(Math.pow(B.X - A.X, 2) + Math.pow(B.Y - A.Y, 2));
+	}
 }
--- Grafo/src/Structure/Vertice.java
+++ Grafo/src/Structure/Vertice.java
@@ -9,4 +9,16 @@ public class Vertice {
 	public String Nome;
 	public double X;
 	public double Y;
+	
+	/**
+	 * Construtor
+	 * @param nome Nome do Vertice
+	 * @param x Coordenada X
+	 * @param y Coordenada Y
+	 */
+	public Vertice(String nome, double x, double y) {
+		this.Nome = nome;
+		this.X = x;
+		this.Y = y;
+	}
 }
